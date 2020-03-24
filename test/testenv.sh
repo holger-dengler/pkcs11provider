4fca851990acc318d75d3f162d5877c88d1bf972
Patrick Steuer <patrick.steuer@de.ibm.com>
2020-03-24 22:53:24 +0100

    testenv: set slot 0 as default for the test suite
    Signed-off-by: Patrick Steuer <patrick.steuer@de.ibm.com>

--- test/testenv.sh
+++ test/testenv.sh
@@ -2,5 +2,5 @@
 
 # set default values
 export PKCS11MODULE=${PKCS11MODULE:-libopencryptoki.so};
-export PKCS11SLOTID=${PKCS11SLOTID:-1};
+export PKCS11SLOTID=${PKCS11SLOTID:-0};
 export PKCS11USERPIN=${PKCS11USERPIN:-87654321};
